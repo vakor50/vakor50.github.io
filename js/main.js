c75d88d1888208237f5b249fa38b47dfb1786cb8
Vir Thakor <thakor.vir@gmail.com>
2017-09-22 11:04:25 -0400

    smooth scrolling fix

--- js/main.js
+++ js/main.js
@@ -1,40 +1,9 @@
-// jQuery to collapse the navbar on scroll
-// $(window).scroll(function() {
-    // if ($(".navbar").offset().top > 50) {
-    //     $(".navbar-fixed-top").addClass("top-nav-collapse");
-    // } else {
-    //     $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    // }
-// });
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-// $(function() {
-    // $('a.page-scroll').bind('click', function(event) {
-    //     console.log($(this).attr('href'));
-    //     var $anchor = $(this);
-    //     if ($(this).attr('href') == '#title') {
-    //         $('html, body').stop().animate({
-    //             scrollTop: 0
-    //         }, 1500, 'easeInOutExpo');
-    //     }
-    //     else {
-    //         $('html, body').stop().animate({
-    //             scrollTop: $($anchor.attr('href')).offset().top
-    //         }, 1500, 'easeInOutExpo');
-    //     }
-    //     event.preventDefault();
-    // });
-// });
-
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
 function checkOffset() {
-    // var element = document.getElementsByClassName('navbar');
-    // var rect = element.getBoundingClientRect();
-    // console.log(rect.top, rect.right, rect.bottom, rect.left);
     if ($(".navbar").offset().top > $("#about").offset().top - 100) {
         $(".navbar-fixed-top").addClass("disappear");
     }     
@@ -49,6 +18,7 @@ $('a[href*="#"]')
     .not('[href="#"]')
     .not('[href="#0"]')
     .click(function(event) {
+        $('html body').stop();
         // On-page links
         if (
             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
@@ -60,13 +30,16 @@ $('a[href*="#"]')
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             // Does a scroll target exist?
 
-                console.log(target.offset().top);
+                // console.log(target.offset().top);
 
             if (target.length) {
 
                 // Only prevent default if animation is actually gonna happen
                 // event.preventDefault();
 
+                // $('html body').animate({
+                //     scrollTop: target.offset().top
+                // }, 2000);
                 $('html body').animate({
                         scrollTop: target.offset().top
                     }, 1000, function() {
